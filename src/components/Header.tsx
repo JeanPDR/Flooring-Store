d00a0af7e3b03ba9309b12aebcf4081eeed049c8
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2024-07-12 21:39:13 -0300

    UPDATE - Modificando Header
    Deixando fixo em todo o site

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,95 @@
-// components/Header.tsx
-import Image from 'next/image';
+"use client";
 
-const Header: React.FC = () => {
+import React, { useState, useEffect, useRef } from "react";
+import Image from "next/image";
+import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+
+const Header = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <header className="flex justify-between items-center bg-gray-100 p-4">
+    <header className="fixed top-0 left-0 right-0 flex justify-between items-center bg-gray-100 p-4 z-50 w-full h-[130px]">
       <div className="flex-1 flex justify-center">
-        <Image src="/images/Transparent-Logo.avif" alt="Logo" width={50} height={50} />
+        <Image
+          src="/images/Transparent-Logo.avif"
+          alt="Logo"
+          width={140}
+          height={80}
+        />
       </div>
-      <a href="mailto:contact@pnvflooringstore.com" className="flex-2 text-center bg-blue-500 text-white py-2 px-4 rounded-md font-bold">
-        contact@pnvflooringstore.com
-      </a>
-      <div className="flex-1 flex justify-center items-center space-x-2">
-        <p className="tracking-wider">Questions? Call</p>
-        <a href="tel:+6562137247" className="text-blue-500 font-bold tracking-wider">656-213-7247</a>
+
+      <div className="flex-1 flex justify-center items-center space-x-6">
+        <div className="relative" ref={dropdownRef}>
+          <button
+            className="tracking-wider cursor-pointer space-x-2m font-bold"
+            onClick={toggleDropdown}
+          >
+            PRODUCTS
+          </button>
+          {isDropdownOpen && (
+            <div className="absolute top-8 left-0 bg-white border rounded shadow-lg">
+              <ul>
+                <li className="px-4 py-4 hover:bg-gray-200 cursor-pointer">
+                  DURAWOOD
+                </li>
+                <li className="px-4 py-4 hover:bg-gray-200 cursor-pointer">
+                  V-EVO MAX
+                </li>
+                <li className="px-4 py-4 hover:bg-gray-200 cursor-pointer">
+                  V-EVO XL
+                </li>
+                <li className="px-4 py-4 hover:bg-gray-200 cursor-pointer">
+                  AZUL TORTUGA
+                </li>
+                <li className="px-4 py-4 hover:bg-gray-200 cursor-pointer">
+                  PANZU
+                </li>
+                <li className="px-4 py-4 hover:bg-gray-200 cursor-pointer">
+                  NUWUD
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <a
+          href="/about"
+          className="tracking-wider cursor-pointer space-x-2 font-bold"
+        >
+          ABOUT
+        </a>
+        <a
+          href="tel:+6562137247"
+          className="text-black tracking-wider font-bold"
+        >
+          <PhoneAndroidIcon className="text-blue-500 " />
+          656-213-7247
+        </a>
+        <button className="bg-blue-500 w-[180px] h-[40px] m-[5px] rounded-[5px] text-white tracking-wider hover:bg-blue-600 font-bold">
+          SCHEDULE NOW
+          <ArrowRightAltIcon />
+        </button>
       </div>
     </header>
   );
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="flex items-center justify-center bg-gray-100 p-8">
+    <section className="mt-[100px] flex items-center justify-center bg-gray-100 p-8">
       <div className="flex flex-col md:flex-row items-center">
         <div className="flex-1 flex justify-center animate-slideInRight">
           <Image
@@ -16,11 +16,19 @@ const HeroSection: React.FC = () => {
           />
         </div>
         <div className="flex-1 md:ml-8 mt-4 md:mt-0 text-left animate-slideInLeft">
-          <h1 className="text-6xl leading-tight" style={{ fontFamily: "'Corben', sans-serif" }}>
+          <h1
+            className="text-6xl leading-tight"
+            style={{ fontFamily: "'Corben', sans-serif" }}
+          >
             Welcome to our Flooring Store
           </h1>
-          <p className="mt-4 text-base text-gray-700" style={{ maxWidth: '400px' }}>
-            Discover the perfect flooring for your home at PNV Flooring. Our wide selection and expert installation services will transform your space.
+          <p
+            className="mt-4 text-base text-gray-700"
+            style={{ maxWidth: "400px" }}
+          >
+            Discover the perfect flooring for your home at PNV Flooring. Our
+            wide selection and expert installation services will transform your
+            space.
           </p>
         </div>
       </div>
